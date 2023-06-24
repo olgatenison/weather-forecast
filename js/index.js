8100b0c410624f4fee04b9197e74750241f564a4
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-06-24 21:47:29 +0300

    js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const apiKey = "b5bd57ab41214874802111150232306";
 // const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
 
 /*Отримуємо значення з форми*/
+const header = document.querySelector(".header");
 const form = document.querySelector("#form");
 
 const input = document.querySelector("#inputCity");
@@ -30,13 +31,38 @@ form.onsubmit = function (e) {
     })
     .then((data) => {
       console.log(data);
-      console.log(data.location.name);
-      console.log(data.location.country);
-      console.log(data.current.temp_c);
-      console.log(data.current.feelslike_c);
-      console.log(data.current.condition.text);
-      console.log(data.current.condition.icon);
+      const city = data.location.name;
+      const country = data.location.country;
+      const temp = data.current.temp_c;
+      const feelsTemp = data.current.feelslike_c;
+      const clouds = data.current.condition.text;
+      const humidity = data.current.condition.humidity;
 
       // відображаємо в картці
+      // розмітка для картки
+      const html = `
+      <div class="main__card card">
+      <p class="card__country">${country}</p>
+      <h2 class="card__city">${city}</h2>
+      <div class="card__weather">
+        <div class="card__left">
+          <div class="card__temp-c">${temp}<span class="up">°C</span></div>
+          <p class="card__condition">${clouds}</p>
+        </div>
+        <div class="card__right">
+          <div class="card__feels">
+            <p class="card__text-f">feels like</p>
+            <div class="card__temp-f">${feelsTemp}<top>°C</top></div>
+          </div>
+          <div class="card__humidity">
+            <p class="card__text-h">humidity</p>
+            <div class="card__temp-h">${humidity} %</div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+
+      //відображаємо картку на сторінці
+      header.insertAdjacentHTML(afterend, html);
     });
 };
