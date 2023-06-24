6c0450bec8e60f307cc5ba8e4ad4e8241b173991
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-06-24 21:09:44 +0300

    css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,7 @@
-// const apiKey = "b5bd57ab41214874802111150232306";
+const apiKey = "b5bd57ab41214874802111150232306";
 // // Replace with your www.weatherapi.com
 
-// const city = "London";
-
 // const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-// // const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
-
-// fetch(url)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//   });
 
 /*Отримуємо значення з форми*/
 const form = document.querySelector("#form");
@@ -22,7 +11,32 @@ const input = document.querySelector("#inputCity");
 let city;
 
 /*Слухаємо відправку форми*/
-form.onsubmit = function () {
-  city = input.value;
-  console.log(city);
+form.onsubmit = function (e) {
+  //відміняємо відправлення форми
+  e.preventDefault();
+
+  // беремо значення з інпуту обрізаємо пробіли
+  city = input.value.trim();
+
+  // робимо запрос на сервер для отримання погоди
+  // адреса запросу
+
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+  //виконуємо запрос
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.location.name);
+      console.log(data.location.country);
+      console.log(data.current.temp_c);
+      console.log(data.current.feelslike_c);
+      console.log(data.current.condition.text);
+      console.log(data.current.condition.icon);
+
+      // відображаємо в картці
+    });
 };
